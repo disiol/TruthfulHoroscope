9b6e363b219af4e8e9f6395bd445e721e9a30d9a
denisimus <s>
2018-04-03 11:07:54 +0300

    Скрол с описанием

--- app/src/main/java/com/denisimusit/truthfulhoroscope/FragmentMenu.java
+++ app/src/main/java/com/denisimusit/truthfulhoroscope/FragmentMenu.java
@@ -1,27 +1,19 @@
 package com.denisimusit.truthfulhoroscope;
 
 import android.app.Fragment;
-import android.app.ListFragment;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Toast;
-
-import java.sql.Array;
 
 public class FragmentMenu extends Fragment implements AdapterView.OnItemClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_frafment_menu, container, false);
+        View view = inflater.inflate(R.layout.faragmet_menu, container, false);
         return view;
     }
 
